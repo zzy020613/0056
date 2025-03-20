7f0bc8525aa5b63c764b23c875bdff44ff68b9a1
zyihjhhjk <15604688+zyihjhhjk@user.noreply.gitee.com>
2025-03-20 23:06:45 +0800

    first commit

---
+++ admin/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app" class="">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+};
+</script>
+
+<style lang="scss">
+*{
+  padding: 0;
+  margin:0;
+}
+html,body{
+  width: 100%;
+  height: 100%;
+  //margin: 0;
+  //padding: 0;
+}
+#app{
+  width: 100%;
+  height:100%;
+}
+
+router-view{
+  width: 100%;
+  height:100%;
+}
+body {
+  padding: 0;
+  margin: 0;
+}
+</style>
